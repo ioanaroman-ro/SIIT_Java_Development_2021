2f30a6435e324904d58aa81fa26b2e34931cc940
Ioana <ioanaroman.ro@gmail.com>
2021-08-26 13:47:33 +0300

    add report

---
+++ Project/siit-web-v4/src/main/java/siit/web/ReportController.java
@@ -0,0 +1,23 @@
+package siit.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+import siit.sevices.ReportService;
+
+@Controller
+public class ReportController {
+    @Autowired
+    ReportService reportService;
+
+    @GetMapping("/generatedReport/{customerId}/{orderId}")
+    public ModelAndView generateReport(@PathVariable int customerId, @PathVariable int orderId){
+        ModelAndView mav = new ModelAndView();
+        reportService.generateReport(customerId, orderId);
+        mav.setViewName("redirect:/customers");
+        return mav;
+    }
+
+}
