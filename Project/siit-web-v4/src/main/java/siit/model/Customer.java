286498e60b712c6ed5f418dc63e510ae77a290d9
Ioana <ioanaroman.ro@gmail.com>
2021-09-16 17:07:09 +0300

    finalize project

--- Project/siit-web-v4/src/main/java/siit/model/Customer.java
+++ Project/siit-web-v4/src/main/java/siit/model/Customer.java
@@ -15,6 +15,14 @@ public class Customer {
     public Customer() {
     }
 
+    public Customer(int id, String name, String phone, String email, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
     public List<Order> getOrders() {
         return orders;
     }
